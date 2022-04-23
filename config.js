cb708be2fa292b51213178521a32f1a49efe948e
okafor-chidimma <chidimma.okafor.c@gmail.com>
2022-04-23 17:49:55 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const Config = {
+  MONGODB:
+    "mongodb+srv://chidimma:0DEFMtQ19dHxQfDO@cluster0.4qvoy.mongodb.net/merng?retryWrites=true&w=majority",
+  SECRET_KEY: "I am secret key",
+};
+export default Config;
